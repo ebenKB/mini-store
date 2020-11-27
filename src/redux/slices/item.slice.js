1d385a85678a7fd7cf299bcf38ba6e4155915a36
ebenKB <eakbo23@gmail.com>
2020-11-27 17:36:37 +0000

    clean up code

--- src/redux/slices/item.slice.js
+++ src/redux/slices/item.slice.js
@@ -1,13 +1,13 @@
 
-import { createSlice } from '@reduxjs/toolkit';
-const itemSlice = createSlice({
-  name: 'counter',
-  initialState: [],
-  reducers: {
+// import { createSlice } from '@reduxjs/toolkit';
+// const itemSlice = createSlice({
+//   name: 'counter',
+//   initialState: [],
+//   reducers: {
 
-  },
+//   },
 
-  addNewItem(state, action) {
+//   addNewItem(state, action) {
 
-  }
-})
+//   }
+// })
